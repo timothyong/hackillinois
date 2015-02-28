6aca3516472e8235a083587f0c7855cf7b9c8ec1
Timothy Ong <tim.ong.118@gmail.com>
2015-02-28 13:18:52 -0600

    start/stop with keyboard

--- webcam.py
+++ webcam.py
@@ -1,11 +1,24 @@
-import cv2, time
+import cv2, time, pygame
 
+pygame.init()
 count = 0;
 while(1):
-    time.sleep(5)
-    webcam = cv2.VideoCapture()
-    webcam.open(0)
-    retval, image = webcam.retrieve()
-    webcam.release()
-    cv2.imwrite("static/images/test{0}.png".format(count), image)
-    count += 1
+    activated = False
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SLASH:
+                activated = True
+    while(activated):
+        time.sleep(5)
+        webcam = cv2.VideoCapture()
+        webcam.open(0)
+        retval, image = webcam.retrieve()
+        webcam.release()
+        cv2.imwrite("static/images/test{0}.png".format(count), image)
+        count += 1
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_MINUS:
+                    activated = False
