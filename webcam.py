8f165f6a0b1ee9cd17f8bebe629a72fae753687f
Timothy Ong <tim.ong.118@gmail.com>
2015-02-28 15:33:47 -0600

    webcam final

--- webcam.py
+++ webcam.py
@@ -1,14 +1,19 @@
-import cv2, time, pygame
+import cv2, time, pygame, os
 
 pygame.init()
 count = 0;
-while(1):
+screen = pygame.display.set_mode((400,400))
+pygame.display.set_caption('Tutorial 1')
+screen.fill((255, 255, 255))
+running = True
+while(running):
     activated = False
     events = pygame.event.get()
     for event in events:
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SLASH:
-                activated = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            activated = True
+        if event.type == pygame.QUIT:
+            running = False
     while(activated):
         time.sleep(5)
         webcam = cv2.VideoCapture()
@@ -20,5 +25,11 @@ while(1):
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_MINUS:
+                if event.key == pygame.K_SPACE:
                     activated = False
+                    running = False
+            if event.type == pygame.QUIT:
+                running = False
+
+#for x in os.listdir('./static/images'):
+#    os.remove('./static/images/{0}'.format(x))
