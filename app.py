d1bc18bf12e4d45c34f13b8ae5a1054de91f2193
Timothy Ong <tim.ong.118@gmail.com>
2015-02-28 15:36:36 -0600

    temporary app. we'll see how its going to run soon

--- app.py
+++ app.py
@@ -1,35 +1,20 @@
 from flask import Flask
-from flask import session,request,render_template, url_for, redirect
-import cv2, time
+from flask import session,request,render_template, url_for, redirect, Response
+import os
+
 
 app = Flask(__name__)
-app.secret_key="lavaryss"
+
 
 @app.route("/")
-def test():
-    count = 0;
+def home():
+    return render_template("index.html")
 
-    '''    time.sleep(5)
-    webcam = cv2.VideoCapture()
-    webcam.open(0)
-    retval, image = webcam.retrieve()
-    webcam.release()
-    cv2.imwrite("test{0}.png".format(count), image)
-    count += 1'''
-    picture()
-    return render_template("test.html")
-
-def picture():
-    count = 0;
-    
-    time.sleep(5)
-    webcam = cv2.VideoCapture()
-    webcam.open(0)
-    retval, image = webcam.retrieve()
-    webcam.release()
-    cv2.imwrite("test{0}.png".format(count), image)
-    count += 1
+@app.route('/results')
+def results():
+    x = os.listdir('./static/images')
+    return render_template("results.html", x)
 
 if __name__ == "__main__":
     app.debug=True
-    app.run(host="0.0.0.0",port=1337)
+    app.run(host="0.0.0.0")
