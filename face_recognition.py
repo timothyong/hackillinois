7c9c564804262b459dd1638e53f438f0b0a88006
Abhishek <abhiver222@gmail.com>
2015-03-01 03:12:10 -0600

    new im vals

--- face_recognition.py
+++ face_recognition.py
@@ -16,25 +16,32 @@ import math as mt
 #	5)If wearing specs then without specs
 #	6)looking left
 #	7)looking right
-ournorms = {'Abhishek':[5916.56,6155.725,5835.83,6033.245,5922.402,6207.052,6028.91],
-			'Akshay':[6268.704,6335.443,6119.169,6277.252,6126.155,6232.754,6294.937],
-			'Chris':[6479.241,6297.295,6477.624,6463.082,6385.727,6275.596,6200.595],
-			'Tim':[6507.45,6569.225,6637.975,6731.95,6546.934,6239.888,6529.477]}
+#ournorms = {'Abhishek':[5916.56,6155.725,5835.83,6033.245,5922.402,6207.052,6028.91],
+#			'Akshay':[6268.704,6335.443,6119.169,6277.252,6126.155,6232.754,6294.937],
+#			'Chris':[6479.241,6297.295,6477.624,6463.082,6385.727,6275.596,6200.595],
+#			'Tim':[6507.45,6569.225,6637.975,6731.95,6546.934,6239.888,6529.477]}
+
+ournorms = {'Abhishek':[5866.278,6229.924,6123.536,5988.862,5966.183,5990.367,5661.118],
+			'Akshay':[6748.139,5658.617,6238.200,6671.678,6228.899,6167.573,5830.901],
+			'Chris':[6312.924,6374.821,6465.274,6275.596,6596.240,6382.099,6456.81], #left right serious
+			'Tim':[6226.022,6010.737,6107.618,6107.386,5994.380,5916.834,7052.4.3]}
 
 indbuffervals = {'Abhishek':100,
 				 'Akshay':100,
 				 'Chris':50,
-				 'Tim':50}
+				 'Tim':150}
 #hardcode values into ournorms above
 
+imagePath = sys.argv[1]
 
-def recognizeFace(faces):
+
+def recognizeFace(image,faces):
 	retval = True
 	if(len(faces)>10):
 		print("Fuck it too many faces shoot everyone")
 		return True, 100
 	for i in range(faces.shape[0]):
-		x, y, w, z = faces[i]
+		x, y, w, h = faces[i]
 		bufw = (400 - w)/2
 		bufh = (400 - h)/2
 		inmod = image[y-bufw:y+w+bufw,x-bufh:x+h+bufh]
@@ -86,18 +93,21 @@ def check(image):
 
 	# Draw a rectangle around the faces
 	for (x, y, w, h) in faces:
-    	cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+		cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    if(len(faces)>0):
-	what, number = recognizeFace(faces)
+	what = True
+
+	if(len(faces)>0):
+		what, number = recognizeFace(image,faces)
+
 	# return what to the arduino
 	if(what is False):
 		print("intruder detected")
 
 
 	cv2.imshow("Faces found", image)
-	#cv2.waitKey(0)
+	cv2.waitKey(0)
 
 
-
+check(imagePath)
 
