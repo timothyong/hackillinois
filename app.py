fdcce39a8021470c463e25616256fc0a110a43d0
Timothy Ong <tim.ong.118@gmail.com>
2015-02-28 22:11:44 -0600

    boootiful front end

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask import session,request,render_template, url_for, redirect, Response
-import os, cv2
+from flask import session,request,render_template, url_for, redirect
+import os
 
 app = Flask(__name__)
 app.secret_key = "lavaryss"
@@ -11,9 +11,9 @@ def home():
 
 @app.route('/results')
 def results():
-    results = []
-    x = os.listdir('./static/images')
-    return render_template("results.html", x)
+    x = os.listdir('static/images')
+    print x
+    return render_template("results.html", x=x)
 
 if __name__ == "__main__":
     app.debug=True
